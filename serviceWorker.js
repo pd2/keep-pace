3673ab27c512ae6f47562275b9155273f7869c67
Glitch (keep-pace) <none>
2025-05-27 10:08:57 +0000

    📻4️⃣ Updated with Glitch

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const staticDev = "Keep PACE"
+const service_worker_version = "v1.0551"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/footprint.txt",
+  "/script.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
